3b39d1919b078122e554fdff743d5bdf373a2342
Aygerim N <nabievaaygerim0@gmail.com>
2023-10-24 17:28:39 +0600

    init

---
+++ main_4.js
@@ -0,0 +1,58 @@
+'use strict'
+
+/*
+1 Напишите скрипт, который считает количество секунд в часе.
+*/
+
+// let secondsInAMinute = 60;
+// let minutesInAnHour = 60;
+// console.log( "Количество секунд в часе", secondsInAMinute * minutesInAnHour);
+
+
+
+ /*
+2 Создайте три переменные - час, минута, секунда. С их помощью выведите текущее время в формате 'час:минута:секунда'.
+*/
+
+// let currentDate = new Date();
+
+// let currentHour = currentDate.getHours();
+// let currentMinute = currentDate.getMinutes();
+// let currentSecond = currentDate.getSeconds();
+
+// let formattedTime = currentHour + ":" + currentMinute + ":" + currentSecond;
+// console.log("Текущее время: " + formattedTime);
+
+
+
+/*
+3 В переменной day лежит какое-то число из интервала от 1 до 31. Определите в какую декаду месяца попадает это число (в первую, вторую или третью).
+*/
+
+// let day = 1;
+
+// if (day >= 1 && day <= 10) {
+//     console.log("Число находится в первой декаде месяца");
+// } else if (day > 10 && day <= 20) {
+//     console.log("Число находится во второй декаде месяца");
+// } else if (day > 20 && day <= 31) {
+//     console.log("Число находится в третьей декаде месяца");
+// }
+
+
+
+/*
+4 В переменной month лежит какое-то число из интервала от 1 до 12. Определите в какую пору года попадает этот месяц (зима, лето, весна, осень).
+*/
+
+// let month = 8
+
+// if (month == 1 || month == 2 || month == 12) {
+//     console.log("Зима");
+// } else if (month >= 3 && month <= 5) {
+//     console.log("Весна");
+// } else if (month >= 6 && month <= 8) {
+//     console.log("Лето");
+// } else if (month >= 9 && month <= 11) {
+//     console.log("Осень");
+// }
